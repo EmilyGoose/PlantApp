2b787c40fda9cebda6dddb0709b4ca51ae0e8db4
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-02-05 14:13:06 -0500

    Implemented RecyclerView in HomeActivity

--- app/src/main/java/com/example/plantapp/HomeActivity.kt
+++ app/src/main/java/com/example/plantapp/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.example.plantapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.plantapp.adapters.PlantListAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -10,13 +13,22 @@ class HomeActivity : AppCompatActivity() {
 
     // Firebase auth - Initialized in onCreate
     private lateinit var auth: FirebaseAuth
+    private lateinit var adapter: PlantListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        // Todo replace this with actual data
+        val plantList = arrayOf("Tree", "Leaf", "I know what plants are trust me")
+        val plantListView = findViewById<RecyclerView>(R.id.plant_list)
+
         // Initialize Firebase Auth
         auth = Firebase.auth
+
+        plantListView.layoutManager = LinearLayoutManager(this)
+        plantListView.adapter = PlantListAdapter(plantList)
+
     }
 
     public override fun onStart() {
