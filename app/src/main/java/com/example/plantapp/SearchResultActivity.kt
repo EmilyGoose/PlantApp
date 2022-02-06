dde61f95e05c8affb07771714d4a92d69a21f218
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-02-05 22:28:20 -0500

    Adding stuff to database works

--- app/src/main/java/com/example/plantapp/SearchResultActivity.kt
+++ app/src/main/java/com/example/plantapp/SearchResultActivity.kt
@@ -2,11 +2,14 @@ package com.example.plantapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
@@ -18,24 +21,40 @@ class SearchResultActivity : AppCompatActivity() {
 
     // Declare later i promise
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_result)
 
-        // Initialize Firebase Auth
+        // Initialize Firebase Auth and Database
         auth = Firebase.auth
+        database = Firebase.database.reference
 
         // Set plant name and title
         val pageData = JSONObject(intent.getStringExtra("data")!!)
-        findViewById<TextView>(R.id.plant_title).text = pageData.getString("title")
-        findViewById<TextView>(R.id.plant_info).text = pageData.getString("extract")
 
-        // Set plant image
+        // Grab the info from the json object
+        val title = pageData.getString("title")
+        val extract = pageData.getString("extract")
         val imgSrc = pageData.getJSONObject("thumbnail").getString("source")
+
+        // Set the views in the page
+        findViewById<TextView>(R.id.plant_title).text = title
+        findViewById<TextView>(R.id.plant_info).text = extract
         Picasso.get().load(imgSrc).into(findViewById<ImageView>(R.id.plant_image));
 
-
+        // "Add to garden" button functionality
+        findViewById<Button>(R.id.button_add_garden).setOnClickListener {
+            // Write the plant to the user's saved plants
+            val userId = auth.currentUser?.uid
+            val userData = database.child("users").child(userId.toString())
+            val plantData = userData.child("Plants").child(title)
+            // Write title and extract
+            plantData.child("title").setValue(title)
+            plantData.child("extract").setValue(extract)
+            plantData.child("thumbnail").setValue(imgSrc)
+        }
     }
 
     public override fun onStart() {
