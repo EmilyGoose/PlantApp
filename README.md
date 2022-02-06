966d977236384b5c542ea5a8c245247d536a3d67
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-02-06 03:13:08 -0500

    Functional photo search

--- README.md
+++ README.md
@@ -36,3 +36,13 @@ evergreen is intended to be a one-stop location for all your plant care and comm
 The scope of evergreen is to be further expanded to implement a community aspect to the application. This would entail being able to find and connect with other nearby users to exchange seeds, grafts, and cuttings of their plants. Additionally we'd implement a system where users can add their personal tips and tricks to the info page for various plants, that other users could then up/down vote to curate the best information for care.
 
 Additionally, the Plant.id API also has the capability to identify diseased plants and provide treatment tips.  This functionality is also to be added in future updates to evergreen, to further aid the user in their tasks to restore the health of their plants with convenience and ease by providing a personalized and comprehensive treatment plan.
+
+# Building the app
+
+This app uses [Firebase](firebase.google.com). Create a new project and enable password authentication, as well as read/write access to the real-time database. Put the `google_services.json` provided into the `app` directory.
+
+This app also uses the [Plant ID API](github.com/flowerchecker/Plant-id-API). Request an api key from them, then create a file named `apikey.properties` in the project root directory with the following:
+
+```properties
+PLANT_API_KEY="apikey"
+```
--- app/src/main/java/com/example/plantapp/PlantSearchActivity.kt
+++ app/src/main/java/com/example/plantapp/PlantSearchActivity.kt
@@ -1,22 +1,36 @@
 package com.example.plantapp
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
 import android.util.Log
+import android.widget.Button
 import android.widget.SearchView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
 import org.json.JSONObject
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+
 
 class PlantSearchActivity : AppCompatActivity() {
 
     // Firebase
     private lateinit var auth: FirebaseAuth
     private lateinit var client: OkHttpClient
+
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +74,12 @@ class PlantSearchActivity : AppCompatActivity() {
 
         // Fix to make whole thing clickable
         searchView.setOnClickListener { searchView.onActionViewExpanded() }
+
+        // Photo search button
+        findViewById<Button>(R.id.photo_button).setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            resultLauncher.launch(cameraIntent)
+        }
     }
 
     public override fun onStart() {
@@ -71,6 +91,72 @@ class PlantSearchActivity : AppCompatActivity() {
             val intent = Intent(this@PlantSearchActivity, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            handleCameraImage(result.data)
+        }
+    }
+
+    private fun handleCameraImage(intent: Intent?) {
+        val bitmap = intent?.extras?.get("data") as Bitmap
+
+        // Convert the bitmap to a byte array
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
+        // Encode the array to b64
+        val encodedImage = Base64.encodeToString(byteArray, Base64.DEFAULT)
+
+        // Create the JSON object for the request
+        val requestData = JSONObject()
+        // REMOVE HARDCODE BEFORE PUSH TO GIT
+        requestData.put("api_key", BuildConfig.PLANT_API_KEY)
+
+        // Add image to request
+        val images = JSONArray().put(encodedImage)
+        requestData.put("images", images)
+
+        // Modifiers https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers
+        val modifiers = JSONArray().put("crops_fast")
+        requestData.put("modifiers", modifiers)
+
+        // Add language
+        requestData.put("plant_language", "en")
+
+        // Plant details https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details
+        val plantDetails = JSONArray().put("common_names")
+        requestData.put("plant_details", plantDetails)
+
+        // Build the request
+        val url = "https://api.plant.id/v2/identify".toHttpUrlOrNull()
+        val urlBuilder = url!!.newBuilder()
+        val requestBuilder = Request.Builder().url(urlBuilder.build())
+        val mediaTypeJson = "application/json; charset=utf-8".toMediaTypeOrNull()
+        val requestBody = requestData.toString().toRequestBody(mediaTypeJson)
+        requestBuilder.post(requestBody)
+        val call = client.newCall(requestBuilder.build())
+
+        // Execute and return request
+        val response = call.enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("PlantSearch", "Request failed. Reason " + e.message)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.let {
+                    // Grab the plant name from the request
+                    // TODO check for errors
+                    val resJSON = JSONObject(it.string())
+                    Log.d("PlantSearch", "Got result from API")
+                    val suggestions = resJSON.getJSONArray("suggestions")
+                    val plantName = suggestions.getJSONObject(0).getString("plant_name")
+
+                    fetchWiki(plantName)
+                }
+            }
+        })
     }
 
     fun fetchWiki(query: String) {
