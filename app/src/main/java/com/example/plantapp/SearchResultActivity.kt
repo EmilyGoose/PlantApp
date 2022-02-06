53f8b95ffd7d4768c15c7783d58e5e7834b81699
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-02-06 04:23:54 -0500

    Deleting plants works

--- app/src/main/java/com/example/plantapp/SearchResultActivity.kt
+++ app/src/main/java/com/example/plantapp/SearchResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.plantapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -13,8 +14,6 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
-
-
 
 
 class SearchResultActivity : AppCompatActivity() {
@@ -38,28 +37,88 @@ class SearchResultActivity : AppCompatActivity() {
         val title = pageData.getString("title")
         val extract = pageData.getString("extract")
         // Get page image at 256px
-        val imgSrc = pageData.getJSONObject("thumbnail").getString("source").replace(Regex("[0-9]+px"), "256px")
+        val imgSrc = pageData.getJSONObject("thumbnail").getString("source")
+            .replace(Regex("[0-9]+px"), "256px")
 
         // Set the views in the page
         findViewById<TextView>(R.id.plant_title).text = title
         findViewById<TextView>(R.id.plant_info).text = extract
         Picasso.get().load(imgSrc).into(findViewById<ImageView>(R.id.plant_image));
 
-        // "Add to garden" button functionality
-        findViewById<Button>(R.id.button_add_garden).setOnClickListener {
-            // Write the plant to the user's saved plants
-            val userId = auth.currentUser?.uid
-            val userData = database.child("users").child(userId.toString())
-            val plantData = userData.child("Plants").child(title)
-            // Write title and extract
-            plantData.child("title").setValue(title)
-            plantData.child("extract").setValue(extract)
-            plantData.child("thumbnail").child("source").setValue(imgSrc)
+        //Check if the user has the plant already
+        database.child("users").child(auth.currentUser?.uid.toString()).get().addOnSuccessListener {
+            val userData = JSONObject(it.value as Map<*, *>)
 
-            // Return the user to their list of plants
-            val intent = Intent(this@SearchResultActivity, HomeActivity::class.java)
-            startActivity(intent)
+            try {
+                // Create an ArrayList<String> to hold all the plant names
+                val plantKeysIterator = userData.getJSONObject("Plants").keys()
+                val plantNames = ArrayList<String>()
+                // Iterate over keys and add corresponding values to list
+                while (plantKeysIterator.hasNext()) {
+                    plantNames.add(
+                        userData.getJSONObject("Plants").getJSONObject(plantKeysIterator.next())
+                            .getString("title")
+                    )
+                }
+
+                // Check if the user already owns the plant
+                if (plantNames.contains(title)) {
+                    // "Remove from button" button functionality
+                    val gardenButton = findViewById<Button>(R.id.button_add_garden)
+
+                    gardenButton.text = getString(R.string.prompt_delete)
+
+                    gardenButton.setOnClickListener {
+                        // Write the plant to the user's saved plants
+                        val userId = auth.currentUser?.uid
+                        val saveData = database.child("users").child(userId.toString())
+                        val plantData = saveData.child("Plants").child(title).removeValue()
+
+                        // Return the user to their list of plants
+                        val intent = Intent(this@SearchResultActivity, HomeActivity::class.java)
+                        startActivity(intent)
+                    }
+                } else {
+                    // "Add to garden" button functionality
+                    findViewById<Button>(R.id.button_add_garden).setOnClickListener {
+                        // Write the plant to the user's saved plants
+                        val userId = auth.currentUser?.uid
+                        val saveData = database.child("users").child(userId.toString())
+                        val plantData = saveData.child("Plants").child(title)
+                        // Write title and extract
+                        plantData.child("title").setValue(title)
+                        plantData.child("extract").setValue(extract)
+                        plantData.child("thumbnail").child("source").setValue(imgSrc)
+
+                        // Return the user to their list of plants
+                        val intent = Intent(this@SearchResultActivity, HomeActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            } catch (e: Exception) {
+                // Duplicate code but it's 4am who cares
+                Log.e("HomeActivity", "Error finding plants. User probably has none")
+                Log.e("HomeActivity", e.message!!)
+                // "Add to garden" button functionality
+                findViewById<Button>(R.id.button_add_garden).setOnClickListener {
+                    // Write the plant to the user's saved plants
+                    val userId = auth.currentUser?.uid
+                    val saveData = database.child("users").child(userId.toString())
+                    val plantData = saveData.child("Plants").child(title)
+                    // Write title and extract
+                    plantData.child("title").setValue(title)
+                    plantData.child("extract").setValue(extract)
+                    plantData.child("thumbnail").child("source").setValue(imgSrc)
+
+                    // Return the user to their list of plants
+                    val intent = Intent(this@SearchResultActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
         }
+
+
     }
 
     public override fun onStart() {
