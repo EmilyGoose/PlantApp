867e17630eeeb8bac94b0c17f7c6b607e286a5ec
philip <philip.jasionowski@gmail.com>
2022-02-05 22:19:51 -0500

    started work on retheming navbar

--- app/src/main/java/com/example/plantapp/MainActivity.kt
+++ app/src/main/java/com/example/plantapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.plantapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
